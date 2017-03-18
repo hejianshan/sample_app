c264c4b50e4fccf9ac8f72bd0d9b0abc40b8b86e
He Jianshan <jianshanhe67@gmail.com>
2017-03-18 09:26:18 +0800

    Integrated with Sublime Text

--- Gemfile
+++ Gemfile
@@ -1,13 +1,14 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'pg', '0.12.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-group :development do
-  gem 'sqlite3', '1.3.5'
+group :development, :test do
   gem 'rspec-rails', '~>2.9.0'
+  gem 'guard-rspec', '0.5.5'
 end
 
 
@@ -26,11 +27,10 @@ end
 gem 'jquery-rails', '2.0.1'
 
 group :test do
+  gem 'rspec-rails', '~>2.9.0'
   gem 'capybara', '1.1.2'
-end
-
-group :production do
-  gem 'pg', '0.12.2'
+  gem 'guard-spork', '0.3.2'
+  gem 'spork', '0.9.0'
 end
 
 # To use ActiveModel has_secure_password
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "Static Pages" do
-  let base_title = "Ruby on Rails Tutorial Sample App"
+  let (:base_title)  { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home page" do
 
@@ -13,9 +13,13 @@ describe "Static Pages" do
     it "should have the right title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                   :text => "#{base_title} | Home")
+                   :text => "Ruby on Rails Tutorial Sample App")
     end
 
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
   end
 
   describe "Help page" do
